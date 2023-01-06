ff3e6fd07640908739019ee2d4be8528cc57bbd5
mys1024 <mys.1024@outlook.com>
2023-01-06 21:17:47 +0800

    chore: 升级依赖

--- src/deps.ts
+++ src/deps.ts
@@ -1,16 +1,20 @@
 export { load } from "https://deno.land/std@0.171.0/dotenv/mod.ts";
 
+export * as colors from "https://deno.land/std@0.171.0/fmt/colors.ts";
+
+export { crypto } from "https://deno.land/std@0.171.0/crypto/mod.ts";
+
+export * as base64 from "https://deno.land/std@0.171.0/encoding/base64.ts";
+
+export { concat } from "https://deno.land/std@0.171.0/bytes/mod.ts";
+
+export * as datetime from "https://deno.land/std@0.171.0/datetime/mod.ts";
+
 export * as oak from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
-export * as colors from "https://deno.land/std@0.168.0/fmt/colors.ts";
-
-export { crypto } from "https://deno.land/std@0.168.0/crypto/mod.ts";
-
-export * as base64 from "https://deno.land/std@0.168.0/encoding/base64.ts";
-
-export { config } from "https://deno.land/std@0.168.0/dotenv/mod.ts";
+export * as djwt from "https://deno.land/x/djwt@v2.8/mod.ts";
 
 export {
   Bson,
@@ -18,9 +22,3 @@ export {
   MongoClient,
   ObjectId,
 } from "https://deno.land/x/mongo@v0.31.1/mod.ts";
-
-export { concat } from "https://deno.land/std@0.168.0/bytes/mod.ts";
-
-export * as djwt from "https://deno.land/x/djwt@v2.8/mod.ts";
-
-export * as datetime from "https://deno.land/std@0.168.0/datetime/mod.ts";
