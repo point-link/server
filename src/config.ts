b5f3b2a7316c42f9130baad26d15c773aaf96c81
mys1024 <mys.1024@outlook.com>
2023-01-06 21:12:33 +0800

    refactor: 优化

--- src/config.ts
+++ src/config.ts
@@ -1,40 +1,58 @@
-import { base64, config } from "./deps.ts";
+import { base64, load } from "./deps.ts";
 
-const dotenv = await config();
+// 加载 .env 文件中的配置到环境变量
+await load({ export: true });
 
-export const DB_HOST = Deno.env.get("DB_HOST") ||
-  dotenv["DB_HOST"];
+// DB_HOST
+const _DB_HOST = Deno.env.get("DB_HOST");
+if (!_DB_HOST) {
+  throw new Error("缺少环境变量: DB_HOST");
+}
+export const DB_HOST = _DB_HOST;
 
-export const DB_NAME = Deno.env.get("DB_NAME") || dotenv["DB_NAME"];
+// DB_NAME
+const _DB_NAME = Deno.env.get("DB_NAME");
+if (!_DB_NAME) {
+  throw new Error("缺少环境变量: DB_NAME");
+}
+export const DB_NAME = _DB_NAME;
 
-export const DB_USERNAME = Deno.env.get("DB_USERNAME") || dotenv["DB_USERNAME"];
+// DB_USERNAME
+const _DB_USERNAME = Deno.env.get("DB_USERNAME");
+if (!_DB_USERNAME) {
+  throw new Error("缺少环境变量: DB_USERNAME");
+}
+export const DB_USERNAME = _DB_USERNAME;
 
-export const DB_PASSWORD = Deno.env.get("DB_PASSWORD") || dotenv["DB_PASSWORD"];
+// DB_PASSWORD
+const _DB_PASSWORD = Deno.env.get("DB_PASSWORD");
+if (!_DB_PASSWORD) {
+  throw new Error("缺少环境变量: DB_PASSWORD");
+}
+export const DB_PASSWORD = _DB_PASSWORD;
 
+// JWT_PRIVATE_KEY
+const _JWT_PRIVATE_KEY = Deno.env.get("JWT_PRIVATE_KEY");
+if (!_JWT_PRIVATE_KEY) {
+  throw new Error("缺少环境变量: JWT_PRIVATE_KEY");
+}
 export const JWT_PRIVATE_KEY = await crypto.subtle.importKey(
   "pkcs8",
-  base64.decode(
-    Deno.env.get("JWT_PRIVATE_KEY") ||
-      dotenv["JWT_PRIVATE_KEY"],
-  ),
-  {
-    name: "ECDSA",
-    namedCurve: "P-256",
-  },
+  base64.decode(_JWT_PRIVATE_KEY),
+  { name: "ECDSA", namedCurve: "P-256" },
   false,
   ["sign"],
 );
 
+// JWT_PUBLIC_KEY
+const _JWT_PUBLIC_KEY = Deno.env.get("JWT_PUBLIC_KEY");
+if (!_JWT_PUBLIC_KEY) {
+  throw new Error("缺少环境变量: JWT_PUBLIC_KEY");
+}
 export const JWT_PUBLIC_KEY = await crypto.subtle.importKey(
   "spki",
-  base64.decode(
-    Deno.env.get("JWT_PUBLIC_KEY") ||
-      dotenv["JWT_PUBLIC_KEY"],
-  ),
-  {
-    name: "ECDSA",
-    namedCurve: "P-256",
-  },
+  base64.decode(_JWT_PUBLIC_KEY),
+  { name: "ECDSA", namedCurve: "P-256" },
   false,
   ["verify"],
 );
--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,5 @@
+export { load } from "https://deno.land/std@0.171.0/dotenv/mod.ts";
+
 export * as oak from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
