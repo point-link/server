22288814340c6dcab7efdb326d0a2e9369aefb06
mys1024 <mys.1024@outlook.com>
2023-01-08 02:06:54 +0800

    refactor: 优化数据库相关环境变量

--- src/config.ts
+++ src/config.ts
@@ -3,33 +3,12 @@ import { base64, load } from "./deps.ts";
 // 加载 .env 文件中的配置到环境变量
 await load({ export: true });
 
-// DB_HOST
-const _DB_HOST = Deno.env.get("DB_HOST");
-if (!_DB_HOST) {
-  throw new Error("缺少环境变量: DB_HOST");
+// DB_URI
+const _DB_URI = Deno.env.get("DB_URI");
+if (!_DB_URI) {
+  throw new Error("缺少环境变量: DB_URI");
 }
-export const DB_HOST = _DB_HOST;
-
-// DB_NAME
-const _DB_NAME = Deno.env.get("DB_NAME");
-if (!_DB_NAME) {
-  throw new Error("缺少环境变量: DB_NAME");
-}
-export const DB_NAME = _DB_NAME;
-
-// DB_USERNAME
-const _DB_USERNAME = Deno.env.get("DB_USERNAME");
-if (!_DB_USERNAME) {
-  throw new Error("缺少环境变量: DB_USERNAME");
-}
-export const DB_USERNAME = _DB_USERNAME;
-
-// DB_PASSWORD
-const _DB_PASSWORD = Deno.env.get("DB_PASSWORD");
-if (!_DB_PASSWORD) {
-  throw new Error("缺少环境变量: DB_PASSWORD");
-}
-export const DB_PASSWORD = _DB_PASSWORD;
+export const DB_URI = _DB_URI;
 
 // JWT_PRIVATE_KEY
 const _JWT_PRIVATE_KEY = Deno.env.get("JWT_PRIVATE_KEY");
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,18 +1,8 @@
 import { MongoClient } from "../deps.ts";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_USERNAME } from "../config.ts";
+import { DB_URI } from "../config.ts";
 import output from "../util/output.ts";
 
 output.info(`正在连接数据库`);
-
-const query = Object.entries({
-  retryWrites: "true",
-  w: "majority",
-  authMechanism: "SCRAM-SHA-1",
-})
-  .map(([k, v]) => `${k}=${v}`)
-  .join("&");
-const uri =
-  `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?${query}`;
-const db = await new MongoClient().connect(uri);
+const db = await new MongoClient().connect(DB_URI);
 
 export default db;
