77811ecb41b987e8d6a771fa11d4aa168f81ddc6
mys1024 <mys.1024@outlook.com>
2023-01-17 04:33:19 +0800

    fix: 尚未启用 net 路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { oak } from "../deps.ts";
 import accountRouter from "./account.ts";
 import friendRouter from "./friend.ts";
 import friendRequestRouter from "./friend_request.ts";
+import netRouter from "./net.ts";
 
 const router = new oak.Router();
 
@@ -21,6 +22,12 @@ router.use(
   "/friend_request",
   friendRequestRouter.routes(),
   friendRequestRouter.allowedMethods(),
+);
+
+router.use(
+  "/net",
+  netRouter.routes(),
+  netRouter.allowedMethods(),
 );
 
 export default router;
