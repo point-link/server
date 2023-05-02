90f0bb9717716b182345092fd92b6c27ce1c2c06
mys1024 <mys.1024@outlook.com>
2023-05-02 22:25:37 +0800

    feat: 支持 IPv6

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,16 @@ if (HTTPS_ENABLE) {
     throw new Error("HTTPS 证书或密钥文件路径为空");
   }
   app.listen({
+    hostname: "[::]",
     port: 443,
     cert: Deno.readTextFileSync(HTTPS_CERT_PATH),
     key: Deno.readTextFileSync(HTTPS_KEY_PATH),
   });
   output.info(`正在监听 443 端口`);
 } else {
-  app.listen({ port: 80 });
+  app.listen({
+    hostname: "[::]",
+    port: 80,
+  });
   output.info(`正在监听 80 端口`);
 }
--- src/router/net.ts
+++ src/router/net.ts
@@ -6,10 +6,13 @@ const router = new oak.Router();
  * 查询当前 HTTP 请求的 IP
  */
 router.get("/ip", (ctx) => {
+  // IPv6 转 IPv4
+  let ip = ctx.request.ip;
+  if (ip.startsWith("::ffff:")) {
+    ip = ip.replace("::ffff:", "");
+  }
   // 响应
-  ctx.response.body = {
-    ip: ctx.request.ip,
-  };
+  ctx.response.body = { ip };
 });
 
 export default router;
